c2c8059b5e817089c80d69920f87fca9486989c2
Felipe Cunha <felipe.cunha@afya.com.br>
2020-09-30 11:32:29 -0300

    icons on tabs bottom

--- src/navegacao/Tab.js
+++ src/navegacao/Tab.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
@@ -8,14 +10,40 @@ const Tab = createBottomTabNavigator();
 
 export default (props) => (
   <Tab.Navigator
-    initialRouteName="TelaB"
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        switch (route.name) {
+          case 'TelaA':
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+            break;
+          case 'TelaB':
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+            break;
+          case 'TelaC':
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+            break;
+        }
+
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
     tabBarOptions={{
       activeTintColor: 'red',
       inactiveTintColor: 'blue',
-      labelStyle: {fontSize: 30},
-    }}>
-    <Tab.Screen name="TelaA" component={TelaA} />
-    <Tab.Screen name="TelaB" component={TelaB} />
-    <Tab.Screen name="TelaC" component={TelaC} />
+      showLabel: true,
+      // labelStyle: { fontSize: 30 }
+    }}
+    initialRouteName="TelaB">
+    <Tab.Screen name="TelaA" component={TelaA} options={{title: 'Inicial'}} />
+    <Tab.Screen name="TelaB" component={TelaB} options={{title: 'Meio'}} />
+    <Tab.Screen name="TelaC" component={TelaC} options={{title: 'Final'}} />
   </Tab.Navigator>
 );
