7980781d4940f2d364ec17d07033bd2c43cdc211
Felipe Cunha <felipe.cunha@afya.com.br>
2020-09-30 09:58:55 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+import TextoCentral from './components/TextoCentral';
+
+export default (props) => (
+  <SafeAreaView style={{flex: 1}}>
+    <TextoCentral>App</TextoCentral>
+  </SafeAreaView>
+);
