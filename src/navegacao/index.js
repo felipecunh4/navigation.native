8564eda4e58833282adf194419b9f2a33afaeb2a
Felipe Cunha <felipe.cunha@afya.com.br>
2020-09-30 11:20:24 -0300

    drawer navigation

--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -3,12 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import Stack from './Stack';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Tab from './Tab';
+import Drawer from './Drawer';
 
 export default (props) => (
   <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
       {/* <Stack /> */}
-      <Tab />
+      {/* <Tab /> */}
+      <Drawer />
     </NavigationContainer>
   </SafeAreaView>
 );
